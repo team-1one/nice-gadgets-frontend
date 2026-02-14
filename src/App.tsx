6565b417be796e01cee64ec4ea077339e0858bc4
Nazar Kundys <nazarrisd@gmail.com>
2026-02-14 16:06:27 +0200

    try PR merge with main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>Hello?</h1>;
+  return (
+    <>
+      <h1>Loading, please wait...</h1>
+      <span>thank you for your patience!</span>
+    </>
+  );
 }
 
 export default App;
